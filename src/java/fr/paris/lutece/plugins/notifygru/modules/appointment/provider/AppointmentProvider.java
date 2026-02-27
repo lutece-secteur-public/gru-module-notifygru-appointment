ab22cdce7d1efc5abdc2b7c06f6381ec99dc0a3d
sebastien leridon <sebleridon@laposte.net>
2026-02-27 11:45:39 +0100

    Revert "LUT26852: improve forms bookmarks"
    This reverts commit 10e1cd3a7afc23027df2cf450a64af1d170b112a.

--- src/java/fr/paris/lutece/plugins/notifygru/modules/appointment/provider/AppointmentProvider.java
+++ src/java/fr/paris/lutece/plugins/notifygru/modules/appointment/provider/AppointmentProvider.java
@@ -291,7 +291,7 @@ public class AppointmentProvider implements IProvider
                 responseRecapValue = StringUtils.EMPTY;
             }
             collectionNotifyMarkers
-                    .add( createMarkerValues( AppointmentNotifyGruConstants.MARK_ENTRY_BASE + response.getEntry( ).getIdEntry( ), responseRecapValue ) );
+                    .add( createMarkerValues( AppointmentNotifyGruConstants.MARK_ENTRY_BASE + response.getEntry( ).getPosition( ), responseRecapValue ) );
         }
         return collectionNotifyMarkers;
     }
@@ -380,7 +380,7 @@ public class AppointmentProvider implements IProvider
             if ( !StringUtils.isEmpty( entry.getTitle( ) ) )
             {
                 collectionNotifyMarkers
-                        .add( createMarkerDescriptions( AppointmentNotifyGruConstants.MARK_ENTRY_BASE + entry.getIdEntry(), null, entry.getTitle( ) ) );
+                        .add( createMarkerDescriptions( AppointmentNotifyGruConstants.MARK_ENTRY_BASE + entry.getPosition( ), null, entry.getTitle( ) ) );
             }
         }
 
